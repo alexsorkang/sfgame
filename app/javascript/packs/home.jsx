37231be1a700bf7b6c96b2ee0bb85d5e693fb9e0
alex sor kang <asork42@berkeley.edu>
2017-07-12 19:40:02 -0700

    added buttons that setstate when pushed

--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -6,17 +6,40 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './app'
+// import joinButton from './joinButton'
+// import createButton from './createButton'
 
 export default class Home extends React.Component {
   constructor(props, _railsContext) {
     super(props);
     // this.state={name:'alex'}
     // this.openToggle = this.openToggle.bind(this);
+    this.buttonToggle = this.buttonToggle.bind(this);
   }
-
+  buttonToggle(button) {
+    this.setState({buttonToggled:button.target.id})
+    console.log(this.state)
+  }
   render() {
     return (
-      <div className="container">Hello {this.props.name}!</div>
+      <div className="container-fluid topMargin">
+
+        <div className="row justify-content-center">
+          <h3>
+            Hello {this.props.name}!
+          </h3>
+        </div>
+
+        <div className="row justify-content-center">
+
+          <div className="btn-group">
+            <button id="joinButton" type="button" className="btn btn-secondary" role="button" aria-pressed="false" onClick={this.buttonToggle}>Join</button>
+            <button id="createButton" type="button" className="btn btn-secondary" role="button" aria-pressed="false" onClick={this.buttonToggle}>Create</button>
+          </div>
+
+        </div>
+
+      </div>
     );
   }
 }
